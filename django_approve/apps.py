ec4742cfaa6d134142dbfaec1dbe711fd999007a
Denis Novikov <alpden550@gmail.com>
2026-06-11 14:57:25 +0300

    feat(app): add Django app configuration for approvals

---
+++ django_approve/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ApproveConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_approve"
+    verbose_name = "Approvals"
