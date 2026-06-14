1a39dcdb96e1bc80666270acd0a5a3ac10c266e8
Denis Novikov <alpden550@gmail.com>
2026-06-14 13:59:22 +0300

    feat(approve): connect `sync_approval_configs` signal on app ready

--- django_approve/apps.py
+++ django_approve/apps.py
@@ -5,3 +5,10 @@ class ApproveConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_approve"
     verbose_name = "Approvals"
+
+    def ready(self):
+        from django.db.models.signals import post_migrate  # noqa: PLC0415
+
+        from django_approve.signals import sync_approval_configs  # noqa: PLC0415
+
+        post_migrate.connect(sync_approval_configs, sender=self)
