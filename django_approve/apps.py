e8dafbd9514ec5be6dc24b315ad1ebeb814c4bae
Denis Novikov <alpden550@gmail.com>
2026-06-15 09:32:42 +0300

    feat(approve): connect `ensure_approval_group` signal on app ready

--- django_approve/apps.py
+++ django_approve/apps.py
@@ -9,6 +9,10 @@ class ApproveConfig(AppConfig):
     def ready(self):
         from django.db.models.signals import post_migrate  # noqa: PLC0415
 
-        from django_approve.signals import sync_approval_configs  # noqa: PLC0415
+        from django_approve.signals import (  # noqa: PLC0415
+            ensure_approval_group,
+            sync_approval_configs,
+        )
 
         post_migrate.connect(sync_approval_configs, sender=self)
+        post_migrate.connect(ensure_approval_group, sender=self)
